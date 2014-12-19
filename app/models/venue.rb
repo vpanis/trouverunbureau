a6429ec0a1a96c3f7228bee956ed54bdeaebb1ea
Ponti Gaston <gaston.ponti@wolox.com.ar>
2014-12-19 18:21:31 -0300

    Vanue validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,5 @@
 class Venue < ActiveRecord::Base
+	# Relations
 	belongs_to :owner, class_name: "User"
 	
 	has_many :spaces, dependent: :destroy
@@ -10,9 +11,10 @@ class Venue < ActiveRecord::Base
 
 	has_many :photos, class_name: "VenuePhoto", dependent: :destroy
 
+  	# Uploaders
   	mount_uploader :logo, LogoUploader
 
-
+  	# Constants/Enums
 	TYPES = [:startup_office, :studio, :corporate_office, :bussines_center, :hotel]
 
 	SPACE_UNIT_TYPES = [:square_mts, :square_foots]
@@ -22,8 +24,57 @@ class Venue < ActiveRecord::Base
 		:pets_allowed, :phone_booth_room, :shared_kitchen, 
 		:wheelchair_accessible]
 
+	# Callbacks
 	after_create :add_owner_to_venue_workers
   	before_destroy :erase_logo
+
+  	# Validations
+  	validates :town, :street, :postal_code, :email, :latitude, :longitude, 
+  		:name, :description, :currency, :v_type, :vat_tax_rate, :owner,
+  		presence: true
+
+	validates :email, format: { 
+		with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+		on: :create 
+	}
+
+	validates :latitude, numericality: { 
+		greater_than_or_equal_to: -90,
+		less_than_or_equal_to: 90
+	}
+
+	validates :longitude, numericality: {
+		greater_than_or_equal_to: 0,
+		less_than_or_equal_to: 180
+	}
+
+	validates :space, :vat_tax_rate, numericality: { 
+		greater_than_or_equal_to: 0 
+	}
+
+	validates :floors, :rooms, :desks, :quantity_reviews, :reviews_sum, numericality: { 
+		only_integer: true,
+		greater_than_or_equal_to: 0 
+	}
+
+	validates :space_unit, inclusion: {
+		in: TYPES
+	}
+
+	validates :space_unit, inclusion: {
+		in: SPACE_UNIT_TYPES
+	}
+
+	validates :day_hours, length: {
+		maximum: 7
+	}
+
+	validates :rating, {
+		greater_than_or_equal_to: 1,
+		less_than_or_equal_to: 5
+	}
+
+	validate :each_amenity_inclusion
 
 	private
 		def add_owner_to_venue_workers
@@ -34,4 +85,11 @@ class Venue < ActiveRecord::Base
 	  		self.remove_logo!
 	  	end
 
+	  	def each_amenity_inclusion
+			if (invalid_amenities = (amenities - AMENITY_TYPES))
+				invalid_amenities.each do |amenity|
+					errors.add(:amenity_list, amenity + " is not a valid amenity")
+				end
+			end
+	  	end
 end
--- app/models/venue_hour.rb
+++ app/models/venue_hour.rb
@@ -1,5 +1,18 @@
 class VenueHour < ActiveRecord::Base
-  belongs_to :venue
+	# Relations
+	belongs_to :venue
 
-  # weekday is the days_to_week_start fn in Time class (Mon:0, Tues:1, ...) 
+	# weekday is the days_to_week_start fn in Time class (Mon:0, Tues:1, ...) 
+
+	# Validations
+	validates :venue, :from, :to, :weekday, presence: true
+
+	validates :weekday, numericality: { 
+		only_integer: true,
+		greater_than_or_equal_to: 0,
+		less_than: 7
+	}
+	
+	# Only one row for an specific day and venue
+	validates :venue_id, uniqueness: { scope: :weekday }
 end
