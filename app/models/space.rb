36b7d0eb74da11634b67cc353df060fed3018c6c
Ponti Gaston <gaston.ponti@wolox.com.ar>
2014-12-19 18:21:54 -0300

    Space validation

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,8 +1,20 @@
 class Space < ActiveRecord::Base
+	# Relations
 	belongs_to :venue
 
 	has_many :bookings
 	belongs_to :photo, class_name: "VenuePhoto"
 
+	# Constants/Enums
 	TYPES = [:conference_room, :meeting_room, :office, :desk]
+
+	# Validations
+	validates :s_type, :name, :capacity, :quantity, :venue, presence: true
+
+	validates :s_type, inclusion: { in: TYPES }
+
+	validates :capacity, :quantity, numericality: { 
+		only_integer: true,
+		greater_than_or_equal_to: 0 
+	}
 end
