e0c66f3db9afe67cc8a3d371497c1b1cd63e8583
Ponti Gaston <gaston.ponti@wolox.com.ar>
2014-12-19 18:21:09 -0300

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	# Relations
 	# Needed to get the venues where this user has permissions but it isn't the owner
 	has_many :venue_workers
 	has_many :venues, through: :venue_workers
@@ -9,4 +10,23 @@ class User < ActiveRecord::Base
 	has_many :spaces, through: :users_favorites
 
 	has_many :bookings
+
+	# Validations
+	validates :first_name, :email, presence: true
+	validates :email, format: { 
+		with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+		on: :create 
+	}, uniqueness: { 
+		case_sensitive: false 
+	}
+
+	validates :quantity_reviews, :reviews_sum, numericality: { 
+		only_integer: true,
+		greater_than_or_equal_to: 0 
+	}
+
+	validates :rating, {
+		greater_than_or_equal_to: 1,
+		less_than_or_equal_to: 5
+	}
 end
