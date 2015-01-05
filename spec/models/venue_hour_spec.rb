3bec27da8b714838f99cf3502f076676afb747b2
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-01-05 11:52:15 -0300

    Venue Hours specs

--- spec/models/venue_hour_spec.rb
+++ spec/models/venue_hour_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe VenueHour, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject { FactoryGirl.create(:venue_hour) }
+
+	# Relations
+	it { should belong_to(:venue) }
+
+	# Presence
+	it { should validate_presence_of(:venue) }
+	it { should validate_presence_of(:from) }
+	it { should validate_presence_of(:to) }
+	it { should validate_presence_of(:weekday) }
+
+	# Numericality
+	it { should validate_numericality_of(:weekday).
+		only_integer.
+		is_less_than(7).
+		is_greater_than_or_equal_to(0) }
+
+	it "should raise exception for repeated days for the same venue" do
+		venue_hours = FactoryGirl.create(:venue_hour)
+		expect { 
+			FactoryGirl.create(:venue_hour, venue: venue_hours.venue, weekday: venue_hours.weekday) 
+		}.to raise_exception
+	end
 end
