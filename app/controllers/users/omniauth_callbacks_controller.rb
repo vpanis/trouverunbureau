9680c66042be889a1a94b1497770ba221ff88d19
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-01-09 14:49:47 -0300

    Add facebook provider in sign up and recover password.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,15 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+module User
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    def facebook
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    # else
-    #   session["devise.facebook_data"] = request.env["omniauth.auth"]
-    #   redirect_to new_user_registration_url
+      return unless @user.persisted?
+
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     end
-  end
 
+  end
 end
