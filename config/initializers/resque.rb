f35e005b39f96e62b02e5a76563ceb86cf3c6ba5
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-02-09 14:55:10 -0300

    Use AppConfiguration to extract yml data.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,11 @@
 require 'resque/server'
 
 # Load the redis configuration from resque.yml
-Resque.redis = YAML.load_file(File.join(Rails.root, "config", "resque.yml"))[Rails.env.to_s]
+resque = AppConfiguration.for('/config/resque')[Rails.env.to_s]
+Resque.redis = Redis.new(host: resque["host"], port: resque["port"], password: resque["password"], thread_safe: true)
 
-resque_auth = AppConfiguration.for(:resque)
 # Resque web auth
+resque_auth = AppConfiguration.for("resque-web")
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
   password == resque_auth.password && user == resque_auth.user
 end
