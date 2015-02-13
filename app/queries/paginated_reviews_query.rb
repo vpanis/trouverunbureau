9bbc3ae00c07238012b1bdad63ab8ed92b6f4162
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-02-13 13:15:15 -0300

    Change Squeel order to the default from ActiveRecord

--- app/queries/paginated_reviews_query.rb
+++ app/queries/paginated_reviews_query.rb
@@ -14,12 +14,12 @@ class PaginatedReviewsQuery < PaginatedQuery
 
   def select_reviews_for_venue(venue_id)
     VenueReview.joins { booking.space } .where { booking.space.venue_id == my { venue_id } }
-      .order { created_at.desc }.includes { booking.owner }
+      .order("created_at desc").includes { booking.owner }
   end
 
   def select_reviews_for_client(client_id, entity)
     ClientReview.joins { booking }
       .where { (booking.owner_type == my { entity }) & (booking.owner_id == my { client_id }) }
-      .order { created_at.desc }.includes { booking.space.venue }
+      .order("created_at desc").includes { booking.space.venue }
   end
 end
