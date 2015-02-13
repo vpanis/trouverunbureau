192743690c51db4fe0e060e78599c1d57bdc683c
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-02-13 14:29:41 -0300

    edit form

---
+++ app/controllers/spaces_controller.rb
@@ -0,0 +1,37 @@
+class SpacesController < ApplicationController
+  inherit_resources
+
+  def edit
+    @space = Space.find(params[:id])
+  end
+
+  def update
+    @space = Space.find(params[:id])
+    @space.update_attributes(space_params)
+    redirect_to edit_space_path(@space)
+  end
+
+  private
+
+  def space_params
+
+    params.require(:space).permit(:s_type, :name, :capacity, :quantity, :description,
+                                  :hour_price, :day_price, :week_price, :month_price)
+  end
+
+  def affinity_points_params
+    params.require(:affinity_points).permit(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, :c10,
+                                            :c11)
+  end
+
+  def affinity_point
+    if @night_club.night_club_affinity_point.present?
+      return @night_club.night_club_affinity_point
+    end
+    affinity = NightClubAffinityPoint.create!(night_club: @night_club)
+    # we initialize all categories to 10% except for category 11.
+    (1..(Affinity::CATEGORIES - 1)).each { |n|affinity.send("c#{n}=", 10) }
+    affinity.save!
+    affinity
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Deskspotting::Application.routes.draw do
 
   resources :venues, only: [:show]
   resources :users, only: [:show, :edit, :update]
+  resources :spaces, only: [:edit, :update]
 
   api_version(module: "api/v1", path: { value: 'api/v1' }) do
     get :spaces, to: 'space#list'
