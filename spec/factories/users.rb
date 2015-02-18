37fec3d236535d81c8fc245aa0c4056e8e3711f9
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-02-18 14:25:13 -0300

    Add a sequence to each email in a factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email }
+    sequence(:email) { |e| "#{e}_#{Faker::Internet.safe_email}" }
     phone { Faker::PhoneNumber.phone_number }
     password { Faker::Internet.password }
     language :en
