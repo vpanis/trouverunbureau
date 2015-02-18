b00b2f683768ee35dfeb741d46533367c1d5616c
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-02-18 12:57:09 -0300

    little refactoring

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,13 +2,14 @@ class SpacesController < ApplicationController
   inherit_resources
 
   def edit
-    return render nothing: true, status: 404 unless Space.find_by(id: params[:id]).present?
-    @space = Space.find(params[:id])
+    @space = Space.find_by(id: params[:id])
+    return render nothing: true, status: 404 unless @space.present?
+
   end
 
   def update
-    return render nothing: true, status: 404 unless Space.find_by(id: params[:id]).present?
-    @space = Space.find(params[:id])
+    @space = Space.find_by(id: params[:id])
+    return render nothing: true, status: 404 unless @space.present?
     return render nothing: true, status: 404 unless @space.can_update(space_params)
     @space.update_attributes(space_params)
     redirect_to edit_space_path(@space)
@@ -20,11 +21,6 @@ class SpacesController < ApplicationController
 
     params.require(:space).permit(:s_type, :name, :capacity, :quantity, :description,
                                   :hour_price, :day_price, :week_price, :month_price)
-  end
-
-  def affinity_points_params
-    params.require(:affinity_points).permit(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, :c10,
-                                            :c11)
   end
 
 end
