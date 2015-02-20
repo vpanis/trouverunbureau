f36529603483162864b2e7ad8ac42cde135ba77c
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-02-20 11:40:26 -0300

    user validations works (tests inclusive)

--- app/contexts/space_context.rb
+++ app/contexts/space_context.rb
@@ -1,13 +1,14 @@
 class SpaceContext
 
-  def initialize(space)
+  def initialize(space, current_user)
     @space = space
+    @current_user = current_user
   end
 
   def can_update?(capacity_param, quantity_param)
     capacity_aux = (capacity_param || @space.capacity).to_i
     quantity_aux = (quantity_param || @space.quantity).to_i
-    valid_quantity?(quantity_aux) && valid_capacity?(capacity_aux)
+    permissions? && valid_quantity?(quantity_aux) && valid_capacity?(capacity_aux)
   end
 
   def valid_quantity?(new_quantity)
@@ -17,7 +18,7 @@ class SpaceContext
                            from: Time.zone.now.at_beginning_of_day - 1.day,
                            to: Time.zone.now.advance(years: 1).at_end_of_day,
                            b_type: Booking.b_types[:month], quantity: delta_quantity,
-                           price: 1, space: @space, owner: User.first }
+                           price: 1, space: @space, owner: @current_user }
     # TODO: el user debe ser el de la sesion
     BookingManager.bookable2?(booking_attributes)
   end
@@ -28,6 +29,10 @@ class SpaceContext
     date_to = Time.zone.now.advance(months: 1).at_end_of_day
     result =  @space.bookings.where { (from >= date_from) & (to <= date_to) }
     !result.present?
+  end
+
+  def permissions?
+    @current_user.present? && @space.venue.owner == @current_user
   end
 
   def update?(space_params)
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,16 +1,21 @@
 class SpacesController < ApplicationController
   inherit_resources
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def edit
     @space = Space.find_by(id: params[:id])
     return render nothing: true, status: 404 unless @space.present?
+    return render nothing: true, status: 403 unless SpaceContext.new(@space, current_user)
+                                                                .permissions?
 
   end
 
   def update
     @space = Space.find_by(id: params[:id])
     return render nothing: true, status: 404 unless @space.present?
-    return render nothing: true, status: 412 unless SpaceContext.new(@space).update?(space_params)
+    space_context = SpaceContext.new(@space, current_user)
+    return render nothing: true, status: 403 unless space_context.permissions?
+    return render nothing: true, status: 412 unless space_context.update?(space_params)
     redirect_to edit_space_path(@space)
   end
 
