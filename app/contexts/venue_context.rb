d155ed319c41a6b3ca41de3f3f44bbfdb38e8e82
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-02-23 17:18:52 -0300

    venue hour logic begining

--- app/contexts/venue_context.rb
+++ app/contexts/venue_context.rb
@@ -5,13 +5,27 @@ class VenueContext
     @current_represented = current_represented
   end
 
+  def can_update?
+    owner? && valid_venue_hours?
+  end
+
   def owner?
     @current_represented.present? && @venue.owner == @current_represented
   end
 
   def update_venue?(venue_params)
-    # return false unless owner?
+    # return false unless can_update?
     @venue.update_attributes!(venue_params)
   end
 
+  def valid_venue_hours?
+    date_from = Time.zone.now.at_beginning_of_day - 1.day
+    no_bookings?(date_from)
+  end
+
+  def no_bookings?(date_from)
+    spaces_id = @venue.spaces.ids
+    Booking.where { (id.in spaces_id) & (from >= date_from) & (state == Booking.states["paid"] ||
+      state == Booking.states["pending_payment"]) }.empty?
+  end
 end
