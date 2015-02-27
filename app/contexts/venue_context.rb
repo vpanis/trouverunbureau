5af98c996f00cd047824e3bbb01bcebfc531e5de
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-02-27 11:26:48 -0300

    refactoring works

--- app/contexts/venue_context.rb
+++ app/contexts/venue_context.rb
@@ -18,7 +18,7 @@ class VenueContext
   def update_venue?(venue_params)
     new_days_hours = venue_params[:day_hours_attributes]
     return false unless can_update?(new_days_hours)
-    @venue.update_attributes!(venue_params)
+    @venue.update_attributes(venue_params)
   end
 
   private
