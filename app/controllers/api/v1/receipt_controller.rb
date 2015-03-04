df3ef48ca56a141b3241ec32a88194193864a78e
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-03-04 16:32:39 -0300

    lalala

--- app/controllers/api/v1/receipt_controller.rb
+++ app/controllers/api/v1/receipt_controller.rb
@@ -17,8 +17,8 @@ module Api
         return record_not_found unless booking.present?
         receipt_context = ReceiptContext.new(current_represented)
         return forbidden unless receipt_context.owner?(booking)
-
-        render_nothing if receipt_context.create_receipt(booking)
+        result = receipt_context.create_receipt(booking)
+        render json: { receipt: ReceiptSerializer.new(result) }, status: 200 if result.present?
       end
     end
   end
