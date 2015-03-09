23af7785e723defe0b12358459a5e95ed924f50c
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-03-09 15:47:21 -0300

    little changes

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,3 @@
 class Country < ActiveRecord::Base
-  has_many :venues
   validates :name, presence: true
 end
--- db/migrate/20150309155158_change_country_to_venue.rb
+++ db/migrate/20150309155158_change_country_to_venue.rb
@@ -1,5 +1,6 @@
 class ChangeCountryToVenue < ActiveRecord::Migration
   def change
+    remove_column :venues, :country, :string
     add_reference :venues, :country, index: true
   end
 end
