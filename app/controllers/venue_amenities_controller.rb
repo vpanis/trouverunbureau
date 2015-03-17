e064ce32c32a4323c66f38b0f00e4b6cecef752d
lucila <lucila.stancato@wolox.com.ar>
2015-03-16 21:55:08 -0300

    save amenitites

--- app/controllers/venue_amenities_controller.rb
+++ app/controllers/venue_amenities_controller.rb
@@ -8,14 +8,14 @@ class VenueAmenitiesController < VenuesController
   def save_amenities
     @venue = Venue.find(params[:id])
     return unless can_edit?
-    @venue.update_attributes!(amenities_params)
+    @venue.update_attributes!(amenities: amenities_params)
     redirect_to photos_venue_path(@venue)
   end
 
   private
 
   def amenities_params
-    params.require(:venue).permit(amenities: [])[:amenities].reject!(&:empty?)
+    params.require(:venue).permit(amenities: [])[:amenities].reject(&:empty?)
   end
 
 end
--- spec/controllers/venue_amenities_controller_spec.rb
+++ spec/controllers/venue_amenities_controller_spec.rb
@@ -75,4 +75,83 @@ describe VenueAmenitiesController do
       end
     end # when the user is not logged in
   end  # GET venues/:id/amenities
+
+  context 'PATCH venues/:id/amenities' do
+    context 'a user is logged in' do
+      before(:each) { sign_in user }
+      after(:each) { sign_out user }
+
+      context 'the venue belongs to the user' do
+        let!(:venue) do
+          create(:venue, owner: user,
+                         amenities: [Venue::AMENITY_TYPES[3].to_s, Venue::AMENITY_TYPES[4].to_s])
+        end
+        let(:new_amenities) do
+          [Venue::AMENITY_TYPES[2].to_s, Venue::AMENITY_TYPES[3].to_s,
+           Venue::AMENITY_TYPES[5].to_s]
+        end
+
+        before do
+          params = { id: venue.id, amenities: new_amenities }
+          patch :save_amenities, id: venue.id, venue: params
+          venue.reload
+        end
+
+        it 'succeeds' do
+          expect(response.status).to eq(302)
+        end
+
+        it 'updates amenities' do
+          expect(venue.amenities).to eq(new_amenities)
+        end
+
+        it 'renders the :photos template' do
+          expect(response.redirect_url).to eq(photos_venue_url(venue))
+        end
+      end
+
+      context 'the venue does not belong to the user' do
+        let!(:venue) { create(:venue) }
+        it 'is forbidden' do
+          patch :save_amenities, id: venue.id
+          expect(response.status).to eq(403)
+        end
+      end
+
+      context 'the venue does not exist' do
+        before { patch :save_amenities, id: -1 }
+
+        it 'fails' do
+          expect(response.status).to eq(404)
+        end
+      end
+    end # a user is logged in
+
+    context 'no user is logged in' do
+      context 'when the venue exists' do
+        let!(:venue) { create(:venue) }
+        before { patch :save_amenities, id: venue.id }
+
+        it 'fails' do
+          expect(response.status).to eq(302)
+        end
+
+        it 'redirects to signin' do
+          expect(response.redirect_url).to eq(new_user_session_url)
+        end
+      end
+
+      context 'when the venue does not exists' do
+        before { patch :save_amenities, id: -1 }
+
+        it 'fails' do
+          expect(response.status).to eq(302)
+        end
+
+        it 'redirects to signin' do
+          expect(response.redirect_url).to eq(new_user_session_url)
+        end
+      end
+    end # when the user is not logged in
+  end
 end
