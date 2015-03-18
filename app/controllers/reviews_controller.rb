bc6c9b6809da4a2b61cc1c8cc26f1560de380770
lucila <lucila.stancato@wolox.com.ar>
2015-03-18 12:03:38 -0300

    reviews creation views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  inherit_resources
+  include RepresentedHelper
+  before_action :authenticate_user!
+
+  #TODO implement properly and TEST
+  def new_client_review
+    @booking = Booking.find(params[:id])
+    @review = ClientReview.new(booking: @booking)
+  end
+
+  def create_client_review
+    @booking = Booking.find(params[:id])
+    redirect_to client_review_booking_path(@booking)
+  end
+
+  def new_venue_review
+    @booking = Booking.find(params[:id])
+    @review = VenueReview.new(booking: @booking)
+  end
+
+  def create_venue_review
+    @booking = Booking.find(params[:id])
+    redirect_to venue_review_booking_path(@booking)
+  end
+
+
+end
