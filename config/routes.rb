1c8588327d1ba497f63128950174f8ae72a9a926
Teresa Fontanella De Santis <teresa.fontanella@wolox.com.ar>
2015-04-07 13:30:23 -0300

    show organization refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Deskspotting::Application.routes.draw do
     end
   end
 
-  resources :organizations, only: [:index, :new, :create, :edit, :update]
+  resources :organizations, only: [:index, :new, :create, :edit, :update, :show]
 
   resources :spaces, only: [:edit, :update, :create, :destroy] do
     collection do
