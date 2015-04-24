2a41e1e638bf0477620be4120aa3c62afa48c97b
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-04-24 17:28:54 -0300

    Add sidekiq conf for docker and heroku.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ require 'sidekiq'
 
 # Load the redis configuration
 redis = AppConfiguration.for(:redis)
+redis_uri = ENV['REDISTOGO_URL'] || "redis://#{ENV.fetch('REDIS_PORT_6379_TCP_ADDR', redis.host)}:#{redis.port}"
 Sidekiq.configure_server do |config|
-  config.redis = { host: redis.host, port: redis.port, password: redis.password }
+  config.redis = { uri: URI.parse(redis_uri) }
 end
