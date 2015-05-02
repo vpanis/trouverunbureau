f8171749087101197f8d42c4775456aa719c02f9
lucila <lucila.stancato@wolox.com.ar>
2015-05-01 23:13:27 -0300

    fixed numeric config var

--- config/initializers/payment.rb
+++ config/initializers/payment.rb
@@ -12,12 +12,12 @@ end
 
 Deskspotting::Application.configure do
   config.payment = OpenStruct.new(
-    deskspotting_fee: deskspotting.fee,
+    deskspotting_fee: deskspotting.fee.to_f,
     braintree: OpenStruct.new(
       merchant_account_id: braintree.merchant_account,
-      time_to_poll_for_escrow_status: braintree.minutes_to_poll_for_escrow_status.minutes,
-      hours_to_poll_for_verify_release: braintree.hours_to_poll_for_verify_release.hours,
-      append_random_to_accounts_ids: braintree.append_random_to_accounts_ids
+      time_to_poll_for_escrow_status: braintree.minutes_to_poll_for_escrow_status.to_i.minutes,
+      hours_to_poll_for_verify_release: braintree.hours_to_poll_for_verify_release.to_i.hours,
+      append_random_to_accounts_ids: braintree.append_random_to_accounts_ids == 'true'
     )
   )
 end
