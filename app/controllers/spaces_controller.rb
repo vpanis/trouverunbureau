1a9cd717dbfe30850a44b49df13a7492db7a9ea1
lucila <lucila.stancato@wolox.com.ar>
2015-05-02 16:58:58 -0300

    redirect after edit space

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -9,21 +9,28 @@ class SpacesController < ModelController
     return render_forbidden unless VenueContext.new(@venue, current_represented).owner?
     @space = Space.new(venue: @venue)
     @space_types_options = space_types_options
+    @previous_page = session[:previous_url]
   end
 
   def create
     space = Space.create!(space_params)
-    redirect_to edit_space_path(space)
+    redirect_to edit_space_path(space, previous_page: params[:previous_page])
   end
 
   def edit
     @space = Space.find(params[:id])
     return render_forbidden unless SpaceContext.new(@space, current_represented).owner?
     @space_types_options = space_types_options
+    @previous_page = params[:previous_page] || session[:previous_url] || edit_space_path(@space)
   end
 
   def update
-    do_update(Space, SpaceContext, 'owner?', 'update_space')
+    @space = Space.find(params[:id])
+    context = SpaceContext.new(@space, current_represented)
+    @previous_page = params[:previous_page] || edit_space_path(@space)
+    return render_forbidden unless context.owner?
+    return redirect_to @previous_page if context.update_space(space_params)
+    redirect_to edit_space_path(@space)
   end
 
   def wishlist
@@ -48,11 +55,6 @@ class SpacesController < ModelController
   end
 
   private
-
-  def object_params
-    params.require(:space).permit(:s_type, :name, :capacity, :quantity, :description,
-                                  :hour_price, :day_price, :week_price, :month_price, :deposit)
-  end
 
   def space_params
     params.require(:space).permit(:s_type, :name, :capacity, :quantity, :description, :deposit,
