b5aa0fde3cee683d12022a492c9c9997565b90ba
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-05-29 18:03:08 -0300

    Worker to send the reviews mails.

---
+++ app/workers/send_reviews_mail_worker.rb
@@ -0,0 +1,30 @@
+class SendReviewsMailWorker
+  include Sidekiq::Worker
+
+  def perform
+    bookings_for_reviews.each do |booking|
+      'send_mail_to_guest' unless booking.client_review.present?
+      'send_mail_to_host' unless booking.venue_review.present?
+    end
+  end
+
+  # Mangopay for now.
+  # Search every booking that the check-in or check-out is in x hours from the current date
+  def bookings_for_reviews
+    Booking.includes(:client_review, :venue_review, :owner, space: [venue: [:owner]])
+      .joins('INNER JOIN spaces ON spaces.id = bookings.space_id')
+      .joins('INNER JOIN venues ON venues.id = spaces.venue_id')
+      .joins('INNER JOIN time_zones ON time_zones.id = venues.time_zone_id')
+      .where("state IN (:states) AND (((bookings.from + (interval '1 second' *
+        time_zones.seconds_utc_difference)) BETWEEN :t1 AND :t2) OR ((bookings.to +
+        (interval '1 second' * time_zones.seconds_utc_difference)) BETWEEN :t1 AND :t2))",
+             states: [Booking.states[:paid], Booking.states[:cancelled], Booking.states[:denied]],
+             t1: Time.current.advance(days: (hours_from_check_in_out + 1) * (-1)),
+             # second less because BETWEEN includes both dates
+             t2: Time.current.advance(days: hours_from_check_in_out * (-1)), seconds: -1)
+  end
+
+  def hours_from_check_in_out
+    Rails.configuration.hours_from_check_in_out_for_rate
+  end
+end
