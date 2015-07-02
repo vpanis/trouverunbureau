20df46e69e5e1e9c6eea330170a1363d10de2fe8
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-07-02 09:54:46 -0300

    Fix venue timezone bug.

--- app/workers/time_zone_retriever_worker.rb
+++ app/workers/time_zone_retriever_worker.rb
@@ -7,7 +7,7 @@ class TimeZoneRetrieverWorker
     t = TimeZone.find_or_initialize_by(zone_identifier: timezone.zone)
     t.seconds_utc_difference = timezone.utc_offset / 60
     t.save
-    venue.update_attributes(time_zone: t)
+    venue.update_attributes(time_zone: t, force_submit: true)
   rescue Timezone::Error::NilZone
     Rails.logger.error("Venue: #{venue_id} has an invalid latitude/longitude")
   end
