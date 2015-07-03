993bab6249829f4519019e2e59bd97b88a1ae5ad
Santiago José Samra <santsamra@gmail.com>
2015-07-03 01:25:59 -0300

    Rubocop

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,15 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Deskspotting Contact",
-      :to => "santiago.samra@wolox.com.ar",
-      :from => "Deskspotting <noreply@deskspotting.com>"
+      subject: 'Deskspotting Contact',
+      to: 'santiago.samra@wolox.com.ar',
+      from: 'Deskspotting <noreply@deskspotting.com>'
     }
   end
 end
