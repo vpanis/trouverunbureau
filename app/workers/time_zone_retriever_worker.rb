6725ac56075a75fb88da6e9efd4ceba472f963ac
Ponti Gaston <gaston.ponti@wolox.com.ar>
2015-07-06 15:31:07 -0300

    Fix mails of review sended every hour.

--- app/workers/time_zone_retriever_worker.rb
+++ app/workers/time_zone_retriever_worker.rb
@@ -5,7 +5,7 @@ class TimeZoneRetrieverWorker
     venue = Venue.find(venue_id)
     timezone = Timezone::Zone.new(lat: venue.latitude, lon: venue.longitude)
     t = TimeZone.find_or_initialize_by(zone_identifier: timezone.zone)
-    t.seconds_utc_difference = timezone.utc_offset / 60
+    t.seconds_utc_difference = timezone.utc_offset
     t.save
     venue.update_attributes(time_zone: t, force_submit: true)
   rescue Timezone::Error::NilZone
