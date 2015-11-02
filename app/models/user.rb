dd88934397744692650c1be87dfa35322b761c0c
Valeria Serber <valeria.serber@wolox.com.ar>
2015-11-02 14:26:16 -0300

    added new locale files

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, LogoUploader
 
   # Enums
-  LANGUAGES = [:en, :fr, :de]
+  LANGUAGES = [:en, :fr, :de, :es, :pt]
   GENDERS = [:f, :m]
   SUPPORTED_NATIONALITIES = Country.all.map { |c| c[1] }
 
