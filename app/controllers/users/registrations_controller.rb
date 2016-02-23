a075b7e1fec21962445b2925104c53d8c1e0341b
Santiago Traversa <santiago.traversa@gmail.com>
2016-02-23 20:32:53 -0300

    [Card #20] Removing creating mangopay user on registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,6 @@ module Users
     def create
       super
       return unless resource.persisted? # user is created successfuly
-      mpa = MangopayPaymentAccount.create(buyer: resource,
-                                          status: MangopayPaymentAccount.statuses[:processing])
-      Payments::Mangopay::PaymentAccountWorker.perform_async(resource.id, mpa.id)
     end
   end
 end
