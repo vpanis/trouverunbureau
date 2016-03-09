5cc72eff6325dc66d2e0ae1a3ec8c422472f0a56
Santiago Traversa <santiago.traversa@gmail.com>
2016-03-09 15:28:44 -0300

    [MISC] Fixing flapping test

--- spec/workers/collection_account_worker_spec.rb
+++ spec/workers/collection_account_worker_spec.rb
@@ -7,10 +7,12 @@ module Payments
 
       it 'should not touch anything if the collection account does not exist' do
         initial_count = MangopayCollectionAccount.count
+        expect(mca.created_at).to eq(mca.updated_at)
 
         subject.perform(-1, {})
 
-        expect(mca.updated_at).to eq(mca.reload.updated_at)
+        mca.reload
+        expect(mca.created_at).to eq(mca.updated_at)
         expect(MangopayCollectionAccount.count).to eq(initial_count)
       end
 
