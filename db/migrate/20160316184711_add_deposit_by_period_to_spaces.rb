f4fccb94b1601451744c2bf1134de961401957ae
Thiago Araujo <worf.mogh@gmail.com>
2016-04-09 00:00:59 -0300

    keep deposit values on migration add_deposit_by_period_to_spaces

--- db/migrate/20160316184711_add_deposit_by_period_to_spaces.rb
+++ db/migrate/20160316184711_add_deposit_by_period_to_spaces.rb
@@ -5,5 +5,16 @@ class AddDepositByPeriodToSpaces < ActiveRecord::Migration
     add_column :spaces, :week_deposit, :integer
     add_column :spaces, :month_deposit, :integer
     add_column :spaces, :month_to_month_deposit, :integer
+
+    Space.where("deposit is NOT NULL and deposit > ?", 0).each do |space|
+      deposit = space.deposit
+
+      space.update_attributes(hour_deposit:            deposit) if space.hour_price.present?
+      space.update_attributes(day_deposit:             deposit) if space.day_price.present?
+      space.update_attributes(week_deposit:            deposit) if space.week_price.present?
+      space.update_attributes(month_deposit:           deposit) if space.month_price.present?
+      space.update_attributes(month_to_month_deposit:  deposit) if space.month_to_month_price.present?
+    end
+
   end
 end
