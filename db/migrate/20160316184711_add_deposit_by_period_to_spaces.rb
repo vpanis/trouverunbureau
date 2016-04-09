1cc6cafdb718d53c9852c50b6a0382a4cf3dbf3c
Thiago Araujo <worf.mogh@gmail.com>
2016-04-09 11:09:52 -0300

    fix act_as_decimal on deposit when migrating

--- db/migrate/20160316184711_add_deposit_by_period_to_spaces.rb
+++ db/migrate/20160316184711_add_deposit_by_period_to_spaces.rb
@@ -7,15 +7,13 @@ class AddDepositByPeriodToSpaces < ActiveRecord::Migration
     add_column :spaces, :month_to_month_deposit, :integer
 
     Space.where("deposit is NOT NULL and deposit > ?", 0).each do |space|
-      deposit = space.deposit
+      deposit = space.deposit.to_f / 100 #fix act_as_decimal 2
 
-      space.hour_deposit            = deposit if space.hour_price.present?
-      space.day_deposit             = deposit if space.day_price.present?
-      space.week_deposit            = deposit if space.week_price.present?
-      space.month_deposit           = deposit if space.month_price.present?
-      space.month_to_month_deposit  = deposit if space.month_to_month_price.present?
-
-      space.save!
+      space.update_attributes(hour_deposit:            deposit) if space.hour_price.present?
+      space.update_attributes(day_deposit:             deposit) if space.day_price.present?
+      space.update_attributes(week_deposit:            deposit) if space.week_price.present?
+      space.update_attributes(month_deposit:           deposit) if space.month_price.present?
+      space.update_attributes(month_to_month_deposit:  deposit) if space.month_to_month_price.present?
     end
 
   end
