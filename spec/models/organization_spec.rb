4a9c42f07fd18775c1acea66a020bc056733b057
Santiago Traversa <santiago.traversa@gmail.com>
2016-04-15 01:51:03 -0300

    [Card-82] Fixing email notifications to organisations
    - also improving organization & organization user specs

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -5,14 +5,35 @@ RSpec.describe Organization, type: :model do
 
   # Relations
   it { should have_many(:organization_users) }
-  it { should have_many(:users) }
+  it { should have_many(:users).through(:organization_users) }
   it { should have_many(:venues) }
   it { should have_many(:bookings) }
+  it { should have_one(:braintree_payment_account) }
+  it { should have_one(:mangopay_payment_account) }
 
   # Presence
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:phone) }
+
+  # Callbacks
+  context 'callbacks' do
+    it { is_expected.to callback(:initialize_fields).after(:initialize) }
+    it { is_expected.to callback(:assign_user).after(:create) }
+  end
+
+  # Numericality
+  it do
+    should validate_numericality_of(:quantity_reviews)
+    .only_integer
+    .is_greater_than_or_equal_to(0)
+  end
+
+  it do
+    should validate_numericality_of(:reviews_sum)
+    .only_integer
+    .is_greater_than_or_equal_to(0)
+  end
 
   it 'should create a OrganizationUser' do
     user = FactoryGirl.create(:user)
