8df7f1c44f7da47e0bd3b1e3c9bba99a8636fd26
Damien Le Thiec <damien.lethiec@gmail.com>
2017-09-22 17:21:32 -0400

    new config carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -47,7 +47,7 @@ CarrierWave.configure do |config|
     }
     config.storage = :fog
     config.fog_use_ssl_for_aws = false
-    config.fog_directory = amazons3.bucket_name
+    config.fog_directory = ENV['AMAZONS3_BUCKET_NAME']
     config.fog_public = true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
